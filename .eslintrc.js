16005b9d475dd19271980c6e8ddea39eb424e527
Joseph Stahl <josephst18+git@outlook.com>
2016-09-07 23:04:27 -0500

    updates

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "extends": "google",
+    "installedESLint": true,
+    "rules": {
+        'linebreak-style': [0, 'unix'], // don't complain about CRLF; Git handles the conversion.
+        'object-curly-spacing': [2, 'always']
+    },
+    "env": {
+        'browser': true,
+        'node': true,
+        'jquery': true
+    },
+    "globals": {
+        'cytoscape': false,
+        'GlyElements': false
+    }
+};
